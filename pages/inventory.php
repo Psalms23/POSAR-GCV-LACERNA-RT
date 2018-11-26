064c73ef7b98f0138ef9dfc78a9cf0db483f800c
jay-dev <creeped12@gmail.com>
2018-11-26 10:46:02 +0800

    reports inventory done

--- pages/inventory.php
+++ pages/inventory.php
@@ -98,7 +98,7 @@ $branch=$_SESSION['branch'];
                     <tbody>
 <?php
 		$branch=$_SESSION['branch'];
-		$query=mysqli_query($con,"select * from product natural join supplier where branch_id='$branch' order by prod_name")or die(mysqli_error());
+		$query=mysqli_query($con,"select * from product where prod_qty<=reorder and branch_id='$branch'")or die(mysqli_error());
 		$grand=0;
 		while($row=mysqli_fetch_array($query)){
 			$total=$row['prod_price']*$row['prod_qty'];
